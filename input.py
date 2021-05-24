0927d525a0f308c216de309509b2f0be318e3048
MSI\Nuser <felix.su@cyweemotion.com>
2021-05-24 10:46:55 +0800

    add input function practice file

---
+++ input.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+name = input("What's your name?")
+age = input("How old are you?")
+gender = input("What's your gender?")
+print("Your name is " + name + ", age is " + age + ", " + gender)
+
+
+# In[ ]:
+
+
+
+
